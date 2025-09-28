0a6dd1f7cf0e47703b009900f4cf61dec7e5c363
mohitSingh <mohitsingh.2626452@gmail.com>
2025-09-28 22:25:02 +0530

    errorOK

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,8 +1,20 @@
+// import { Module } from '@nestjs/common';
+// import { CommentsController } from './comments.controller';
+// import { CommentsService } from './comments.service';
+
+// @Module({
+//   controllers: [CommentsController],
+//   providers: [CommentsService],
+// })
+// export class CommentsModule {}
+
 import { Module } from '@nestjs/common';
-import { CommentsController } from './comments.controller';
 import { CommentsService } from './comments.service';
+import { CommentsController } from './comments.controller';
+import { AnnouncementsModule } from '../announcements/announcements.module'; // ✅ import
 
 @Module({
+  imports: [AnnouncementsModule], // ✅ allow injection of AnnouncementsService
   controllers: [CommentsController],
   providers: [CommentsService],
 })
